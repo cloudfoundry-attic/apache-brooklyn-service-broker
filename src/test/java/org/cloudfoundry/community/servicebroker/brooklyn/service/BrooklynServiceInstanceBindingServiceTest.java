17bf80b1a2cc24022b4a51ad248c0d4ee91bb060
Robert Moss <robertgmoss@gmail.com>
2015-07-10 10:37:32 +0100

    add generic types to call to emptyMap

--- src/test/java/org/cloudfoundry/community/servicebroker/brooklyn/service/BrooklynServiceInstanceBindingServiceTest.java
+++ src/test/java/org/cloudfoundry/community/servicebroker/brooklyn/service/BrooklynServiceInstanceBindingServiceTest.java
@@ -53,7 +53,7 @@ private final static String SVC_INST_BIND_ID = "serviceInstanceBindingId";
 	public void newServiceInstanceBindingCreatedSuccessfully() 
 			throws ServiceBrokerException, ServiceInstanceBindingExistsException {
 
-		when(admin.getApplicationSensors(any(String.class))).thenReturn(Collections.emptyMap());
+		when(admin.getApplicationSensors(any(String.class))).thenReturn(Collections.<String, Object>emptyMap());
 		when(instanceRepository.findOne(any())).thenReturn(serviceInstance);
 		CreateServiceInstanceBindingRequest request = new CreateServiceInstanceBindingRequest(serviceInstance.getServiceDefinitionId(), "planId", "appGuid");
 		ServiceInstanceBinding binding = bindingService.createServiceInstanceBinding(request.withBindingId(SVC_INST_BIND_ID));
@@ -68,7 +68,7 @@ private final static String SVC_INST_BIND_ID = "serviceInstanceBindingId";
 		
 		when(bindingRepository.findOne(any(String.class)))
 		.thenReturn(ServiceInstanceBindingFixture.getServiceInstanceBinding());	
-		when(admin.getApplicationSensors(any(String.class))).thenReturn(Collections.emptyMap());
+		when(admin.getApplicationSensors(any(String.class))).thenReturn(Collections.<String, Object>emptyMap());
 		CreateServiceInstanceBindingRequest request = new CreateServiceInstanceBindingRequest(serviceInstance.getServiceDefinitionId(), "planId", "appGuid");
 				
 		bindingService.createServiceInstanceBinding(request.withBindingId(SVC_INST_BIND_ID));
