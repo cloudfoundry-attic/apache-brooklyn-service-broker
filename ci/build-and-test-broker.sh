dd617c6312affc3c87ba3522ee7bce8158146c0c
Robert Moss <robert.moss@cloudsoftcorp.com>
2016-09-13 17:31:35 +0100

    update build script to rename built file

--- ci/build-and-test-broker.sh
+++ ci/build-and-test-broker.sh
@@ -5,5 +5,5 @@ export TERM=dumb
 cd brooklyn-broker
 
 ./gradlew clean build
-
-mv build/libs/brooklyn-service-broker.war build/libs/brooklyn-broker-$(cat release/tag).war
+ls build/libs
+mv build/libs/brooklyn-broker.war build/libs/brooklyn-broker-$(cat release/tag).war
