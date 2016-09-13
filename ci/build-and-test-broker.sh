226184fde2979a07d070dd8f13bc46ffe56ed292
Robert Moss <robert.moss@cloudsoftcorp.com>
2016-09-13 17:54:23 +0100

    output dir for build

--- ci/build-and-test-broker.sh
+++ ci/build-and-test-broker.sh
@@ -5,4 +5,4 @@ export TERM=dumb
 cd brooklyn-broker
 
 ./gradlew clean build
-mv build/libs/brooklyn-broker.war built-brooklyn-broker/brooklyn-broker-$(cat release/tag).war
+mv build/libs/brooklyn-broker.war ../built-brooklyn-broker/brooklyn-broker-$(cat release/tag).war
