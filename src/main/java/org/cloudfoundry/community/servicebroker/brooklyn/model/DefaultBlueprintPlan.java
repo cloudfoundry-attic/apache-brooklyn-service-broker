ad89fd512afe0e3c8a506e314cbe5546edfc3d3c
Andrea Turli <andrea.turli@gmail.com>
2017-01-25 17:02:04 +0100

    add unit test for bind-service failure

--- src/main/java/org/cloudfoundry/community/servicebroker/brooklyn/model/DefaultBlueprintPlan.java
+++ src/main/java/org/cloudfoundry/community/servicebroker/brooklyn/model/DefaultBlueprintPlan.java
@@ -2,71 +2,70 @@ package org.cloudfoundry.community.servicebroker.brooklyn.model;
 
 import java.util.Map;
 
-import org.apache.brooklyn.util.collections.MutableList;
 import org.apache.brooklyn.util.collections.MutableMap;
 import org.apache.brooklyn.util.exceptions.Exceptions;
-import org.springframework.cloud.servicebroker.model.CreateServiceInstanceRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.servicebroker.model.CreateServiceInstanceRequest;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 
-public class DefaultBlueprintPlan extends BlueprintPlan{
-	
-	private static final Logger LOG = LoggerFactory.getLogger(DefaultBlueprintPlan.class);
-	private String appName;
+public class DefaultBlueprintPlan extends BlueprintPlan {
 
-	public DefaultBlueprintPlan(String id, String name, String description, String appName,
-			Map<String, Object> metadata) {
-		super(id, name, description, metadata);
-		this.appName = appName;
-	}
+   private static final Logger LOG = LoggerFactory.getLogger(DefaultBlueprintPlan.class);
+   private String appName;
 
-	@Override
-	public String toBlueprint(String brooklynCatalogId, String location, 
-			CreateServiceInstanceRequest request) {
-		ObjectWriter writer = new ObjectMapper().writer();
-		
-		Map<String, Object> metadata = MutableMap.copyOf(getMetadata());
-		String name = (appName + " (CF Service)").trim();
-        try {
-			if (metadata.containsKey("location")) {
-				location = writer.writeValueAsString(metadata.remove("location"));
-			} else {
-				location = writer.writeValueAsString(location);
-			}
-			
-		} catch (JsonProcessingException e) {
-        	LOG.error("unable to make location: {}",  e.getMessage());
+   public DefaultBlueprintPlan(String id, String name, String description, String appName,
+                               Map<String, Object> metadata) {
+      super(id, name, description, metadata);
+      this.appName = appName;
+   }
+
+   @Override
+   public String toBlueprint(String brooklynCatalogId, String location,
+                             CreateServiceInstanceRequest request) {
+      ObjectWriter writer = new ObjectMapper().writer();
+
+      Map<String, Object> metadata = MutableMap.copyOf(getMetadata());
+      String name = (appName + " (CF Service)").trim();
+      try {
+         if (metadata.containsKey("location")) {
+            location = writer.writeValueAsString(metadata.remove("location"));
+         } else {
+            location = writer.writeValueAsString(location);
+         }
+
+      } catch (JsonProcessingException e) {
+         LOG.error("unable to make location: {}", e.getMessage());
+         Exceptions.propagateIfFatal(e);
+      }
+
+      Map<Object, Object> config = MutableMap.of();
+      // add parameters
+      // TODO sanitize this input from user
+
+      Map<?, ?> parameters = request.getParameters();
+      if (parameters != null) {
+         config.putAll(parameters);
+      }
+      config.putAll(metadata);
+      String blueprint = "";
+      if (config.keySet().size() > 0) {
+         String configJson = null;
+         try {
+            configJson = writer.writeValueAsString(config);
+            blueprint = String.format("{\"name\":\"%s\", \"services\":[{\"type\": \"%s\"}], \"locations\": [%s], \"brooklyn.config\":%s}", name, brooklynCatalogId, location, configJson);
+         } catch (JsonProcessingException e) {
+            LOG.error("unable to add config: {}", e.getMessage());
             Exceptions.propagateIfFatal(e);
-		}
-        
-        Map<Object, Object> config = MutableMap.of();
-        // add parameters
-        // TODO sanitize this input from user
-        
-		Map<?, ?> parameters = request.getParameters();
-		if(parameters != null){
-			config.putAll(parameters);
-		}
-        config.putAll(metadata);
-        String blueprint = "";
-        if (config.keySet().size() > 0) {      
-            String configJson = null;
-            try {
-                configJson = writer.writeValueAsString(config);
-                blueprint = String.format("{\"name\":\"%s\", \"services\":[{\"type\": \"%s\"}], \"locations\": [%s], \"brooklyn.config\":%s}", name, brooklynCatalogId, location, configJson);
-            } catch (JsonProcessingException e) {
-            	LOG.error("unable to add config: {}",  e.getMessage());
-                Exceptions.propagateIfFatal(e);
-            }
-        } else {
-        	blueprint = String.format("{\"name\":\"%s\", \"services\":[{\"type\": \"%s\"}], \"locations\": [%s]}", name, brooklynCatalogId, location);
-        }
-        
-        return blueprint;
-	}
+         }
+      } else {
+         blueprint = String.format("{\"name\":\"%s\", \"services\":[{\"type\": \"%s\"}], \"locations\": [%s]}", name, brooklynCatalogId, location);
+      }
+
+      return blueprint;
+   }
 
 }
--- src/main/java/org/cloudfoundry/community/servicebroker/brooklyn/service/BrooklynServiceInstanceBindingService.java
+++ src/main/java/org/cloudfoundry/community/servicebroker/brooklyn/service/BrooklynServiceInstanceBindingService.java
@@ -33,133 +33,138 @@ import com.google.gson.JsonElement;
 
 @Service
 public class BrooklynServiceInstanceBindingService implements
-		ServiceInstanceBindingService {
-    
-    private static final Logger LOG = LoggerFactory.getLogger(BrooklynServiceInstanceBindingService.class);
-    
-	private BrooklynRestAdmin admin;
-	private BrooklynServiceInstanceBindingRepository bindingRepository;
-    private BrooklynServiceInstanceRepository instanceRepository;
-    private BrooklynCatalogService catalogService;
+        ServiceInstanceBindingService {
 
-    @Autowired
-	public BrooklynServiceInstanceBindingService(BrooklynRestAdmin admin, BrooklynServiceInstanceBindingRepository bindingRepository, BrooklynServiceInstanceRepository instanceRepository, BrooklynCatalogService catalogService) {
-		this.admin = admin;
-		this.bindingRepository = bindingRepository;
-        this.instanceRepository = instanceRepository;
-        this.catalogService = catalogService;
-    }
+   private static final Logger LOG = LoggerFactory.getLogger(BrooklynServiceInstanceBindingService.class);
 
-	protected BrooklynServiceInstanceBinding getServiceInstanceBinding(String bindingId) {
-		return bindingRepository.findOne(bindingId);
-	}
+   private BrooklynRestAdmin admin;
+   private BrooklynServiceInstanceBindingRepository bindingRepository;
+   private BrooklynServiceInstanceRepository instanceRepository;
+   private BrooklynCatalogService catalogService;
 
-	@Override
-	public CreateServiceInstanceBindingResponse createServiceInstanceBinding(CreateServiceInstanceBindingRequest request) {
-		
-		BrooklynServiceInstanceBinding serviceInstanceBinding = getServiceInstanceBinding(request.getBindingId());
-		if (serviceInstanceBinding != null) {
-			throw new ServiceInstanceBindingExistsException(serviceInstanceBinding.getServiceInstanceId(), request.getBindingId());
-		}
-		
+   @Autowired
+   public BrooklynServiceInstanceBindingService(BrooklynRestAdmin admin, BrooklynServiceInstanceBindingRepository bindingRepository, BrooklynServiceInstanceRepository instanceRepository, BrooklynCatalogService catalogService) {
+      this.admin = admin;
+      this.bindingRepository = bindingRepository;
+      this.instanceRepository = instanceRepository;
+      this.catalogService = catalogService;
+   }
+
+   protected BrooklynServiceInstanceBinding getServiceInstanceBinding(String bindingId) {
+      return bindingRepository.findOne(bindingId);
+   }
+
+   @Override
+   public CreateServiceInstanceBindingResponse createServiceInstanceBinding(CreateServiceInstanceBindingRequest request) {
+
+      BrooklynServiceInstanceBinding serviceInstanceBinding = getServiceInstanceBinding(request.getBindingId());
+      if (serviceInstanceBinding != null) {
+         throw new ServiceInstanceBindingExistsException(serviceInstanceBinding.getServiceInstanceId(), request.getBindingId());
+      }
+
       BrooklynServiceInstance serviceInstance = instanceRepository.findOne(request.getServiceInstanceId(), false);
-		String entityId = serviceInstance.getEntityId();
-		
-		LOG.info("creating service binding: [entity={}, serviceDefinitionId={}, bindingId={}, serviceInstanceId={}, appGuid={}", 
-		      entityId, request.getServiceDefinitionId(), request.getBindingId(), request.getServiceInstanceId(), request.getAppGuid()
-        );
+      String entityId = serviceInstance.getEntityId();
 
-        ServiceDefinition service = catalogService.getServiceDefinition(request.getServiceDefinitionId());
-        Predicate<String> sensorWhitelistPredicate = x -> true;
-        Predicate<String> entityBlacklistPredicate = x -> true;
-        Predicate<String> sensorBlacklistPredicate = x -> true;
-        Predicate<String> entityWhitelistPredicate = x -> true;
-        Object planYamlObject = service.getMetadata().get("planYaml");
-        if (planYamlObject != null) {
-            Object rootElement = Iterables.getOnlyElement(Yamls.parseAll(String.valueOf(planYamlObject)));
-			if (rootElement instanceof Map) {
-				sensorWhitelistPredicate = getSensorWhitelistPredicate(rootElement);
-				sensorBlacklistPredicate = getSensorBlacklistPredicate(rootElement);
-				entityWhitelistPredicate = getEntityWhitelistPredicate(rootElement);
-				entityBlacklistPredicate = getEntityBlacklistPredicate(rootElement);
-			}
-        }
-        
-		String childEntityId = null;
-		if (ServiceUtil.getFutureValueLoggingError(admin.hasEffector(entityId, entityId, "bind"))) {
-			Future<String> effector = admin.invokeEffector(entityId, entityId, "bind", "never", request.getParameters() != null ? request.getParameters() : ImmutableMap.of());
-			String bindResponse = ServiceUtil.getFutureValueLoggingError(effector);
-			LOG.info("Calling bind effector: {}", bindResponse);
-			JsonElement jsonElement = JsonFunctions.asJson().apply(bindResponse);
-			if (jsonElement instanceof JsonArray) {
-				childEntityId = ((JsonArray) jsonElement).get(0).getAsString();
-			} else {
-				childEntityId = jsonElement.getAsString();
-			}
-		}
+      LOG.info("creating service binding: [entity={}, serviceDefinitionId={}, bindingId={}, serviceInstanceId={}, appGuid={}",
+              entityId, request.getServiceDefinitionId(), request.getBindingId(), request.getServiceInstanceId(), request.getAppGuid()
+      );
+
+      ServiceDefinition service = catalogService.getServiceDefinition(request.getServiceDefinitionId());
+      Predicate<String> sensorWhitelistPredicate = x -> true;
+      Predicate<String> entityBlacklistPredicate = x -> true;
+      Predicate<String> sensorBlacklistPredicate = x -> true;
+      Predicate<String> entityWhitelistPredicate = x -> true;
+      Object planYamlObject = service.getMetadata().get("planYaml");
+      if (planYamlObject != null) {
+         Object rootElement = Iterables.getOnlyElement(Yamls.parseAll(String.valueOf(planYamlObject)));
+         if (rootElement instanceof Map) {
+            sensorWhitelistPredicate = getSensorWhitelistPredicate(rootElement);
+            sensorBlacklistPredicate = getSensorBlacklistPredicate(rootElement);
+            entityWhitelistPredicate = getEntityWhitelistPredicate(rootElement);
+            entityBlacklistPredicate = getEntityBlacklistPredicate(rootElement);
+         }
+      }
+
+      String childEntityId = null;
+      String bindResponse;
+      Map<String, Object> parameters = request.getParameters() != null ? request.getParameters() : ImmutableMap.of();
+      if (ServiceUtil.getFutureValueLoggingError(admin.hasEffector(entityId, entityId, "bind"))) {
+         Future<String> effector = admin.invokeEffector(entityId, entityId, "bind", "never", parameters);
+         bindResponse = ServiceUtil.getFutureValueLoggingError(effector);
+         if (bindResponse == null) {
+            throw new RuntimeException(String.format("cannot invoke bind effector on entity %s with %s", entityId, Iterables.toString(request.getParameters().entrySet())));
+         }
+         LOG.info("calling bind effector on entity {} with {}: {}", entityId, Iterables.toString(parameters.entrySet()), bindResponse);
+         JsonElement jsonElement = JsonFunctions.asJson().apply(bindResponse);
+         if (jsonElement instanceof JsonArray) {
+            childEntityId = ((JsonArray) jsonElement).get(0).getAsString();
+         } else {
+            childEntityId = jsonElement.getAsString();
+         }
+      }
       Future<Map<String, Object>> credentialsFuture = admin.getCredentialsFromSensors(entityId, MoreObjects.firstNonNull(childEntityId, entityId), sensorWhitelistPredicate, sensorBlacklistPredicate, entityWhitelistPredicate, entityBlacklistPredicate);
       Map<String, Object> credentials = ServiceUtil.getFutureValueLoggingError(credentialsFuture);
-		LOG.info("credentials: {}", Iterables.toString(credentials.entrySet()));
+      LOG.info("credentials: {}", Iterables.toString(credentials.entrySet()));
       serviceInstanceBinding = new BrooklynServiceInstanceBinding(request.getBindingId(), request.getServiceInstanceId(), null, request.getAppGuid(), childEntityId);
-		bindingRepository.save(serviceInstanceBinding);
-		return new CreateServiceInstanceAppBindingResponse().withCredentials(credentials);
-	}
+      bindingRepository.save(serviceInstanceBinding);
+      return new CreateServiceInstanceAppBindingResponse().withCredentials(credentials);
+   }
 
-    @VisibleForTesting
-    public static Predicate<String> getContainsItemInSectionPredicate(Object rootElement, String section, boolean ifAbsent) {
-        return s -> containsItemInSection(s, (Map<?, ?>) rootElement, section, ifAbsent);
-    }
-    
-    private static Boolean containsItemInSection(Object item, Map<?, ?> map, String section, boolean ifAbsent){
-    	Map<?, ?> brooklynConfig = (Map<?, ?>) map.get("brooklyn.config");
-    	Map<?, ?> brokerConfig = (Map<?, ?>) getValue(brooklynConfig, "broker.config");
-		List<?> list = (List<?>) getValue(brokerConfig, section);
-		if(list == null) return ifAbsent;
-		return list.contains(item);
-    }
-    
-    private static Object getValue(Map<?, ?> map, String key){
-    	return (map == null || !map.containsKey(key)) ? null : map.get(key);
-    }
+   @VisibleForTesting
+   public static Predicate<String> getContainsItemInSectionPredicate(Object rootElement, String section, boolean ifAbsent) {
+      return s -> containsItemInSection(s, (Map<?, ?>) rootElement, section, ifAbsent);
+   }
 
-    @VisibleForTesting
-    public static Predicate<String> getSensorWhitelistPredicate(Object rootElement) {
-    	return getContainsItemInSectionPredicate(rootElement, "sensor.whitelist", true);
-    }
-    
-    @VisibleForTesting
-    public static Predicate<String> getSensorBlacklistPredicate(Object rootElement) {
-    	return getContainsItemInSectionPredicate(rootElement, "sensor.blacklist", false).negate();
-    }
+   private static Boolean containsItemInSection(Object item, Map<?, ?> map, String section, boolean ifAbsent) {
+      Map<?, ?> brooklynConfig = (Map<?, ?>) map.get("brooklyn.config");
+      Map<?, ?> brokerConfig = (Map<?, ?>) getValue(brooklynConfig, "broker.config");
+      List<?> list = (List<?>) getValue(brokerConfig, section);
+      if (list == null) return ifAbsent;
+      return list.contains(item);
+   }
 
-    @VisibleForTesting
-    public static Predicate<String> getEntityWhitelistPredicate(Object rootElement){
-    	return getContainsItemInSectionPredicate(rootElement, "entity.whitelist", true);
-    }
+   private static Object getValue(Map<?, ?> map, String key) {
+      return (map == null || !map.containsKey(key)) ? null : map.get(key);
+   }
 
-    @VisibleForTesting
-    public static Predicate<String> getEntityBlacklistPredicate(Object rootElement){
-    	return getContainsItemInSectionPredicate(rootElement, "entity.blacklist", false).negate();
-    }
+   @VisibleForTesting
+   public static Predicate<String> getSensorWhitelistPredicate(Object rootElement) {
+      return getContainsItemInSectionPredicate(rootElement, "sensor.whitelist", true);
+   }
 
-	@Override
-	public void deleteServiceInstanceBinding(DeleteServiceInstanceBindingRequest request)
-			throws ServiceBrokerException {
-		
-		String bindingId = request.getBindingId();
-        BrooklynServiceInstanceBinding serviceInstanceBinding = getServiceInstanceBinding(bindingId);
-        if (serviceInstanceBinding != null) {
-            BrooklynServiceInstance serviceInstance = instanceRepository.findOne(serviceInstanceBinding.getServiceInstanceId(), false);
-			String appId = serviceInstance.getEntityId();
-			String entityId = serviceInstanceBinding.getEntityId();
-			if (entityId==null) entityId = appId;
-			Future<String> effector = admin.invokeEffector(appId, entityId, "unbind", "0", ImmutableMap.<String, Object>of(
-					"app_guid", serviceInstanceBinding.getAppGuid()
-			));
-    		LOG.info("Calling unbind effector: {}", ServiceUtil.getFutureValueLoggingError(effector));
-		    LOG.info("Deleting service binding: [BindingId={}]", bindingId);
-			bindingRepository.delete(bindingId);
-		}
-	}
+   @VisibleForTesting
+   public static Predicate<String> getSensorBlacklistPredicate(Object rootElement) {
+      return getContainsItemInSectionPredicate(rootElement, "sensor.blacklist", false).negate();
+   }
+
+   @VisibleForTesting
+   public static Predicate<String> getEntityWhitelistPredicate(Object rootElement) {
+      return getContainsItemInSectionPredicate(rootElement, "entity.whitelist", true);
+   }
+
+   @VisibleForTesting
+   public static Predicate<String> getEntityBlacklistPredicate(Object rootElement) {
+      return getContainsItemInSectionPredicate(rootElement, "entity.blacklist", false).negate();
+   }
+
+   @Override
+   public void deleteServiceInstanceBinding(DeleteServiceInstanceBindingRequest request)
+           throws ServiceBrokerException {
+
+      String bindingId = request.getBindingId();
+      BrooklynServiceInstanceBinding serviceInstanceBinding = getServiceInstanceBinding(bindingId);
+      if (serviceInstanceBinding != null) {
+         BrooklynServiceInstance serviceInstance = instanceRepository.findOne(serviceInstanceBinding.getServiceInstanceId(), false);
+         String appId = serviceInstance.getEntityId();
+         String entityId = serviceInstanceBinding.getEntityId();
+         if (entityId == null) entityId = appId;
+         Future<String> effector = admin.invokeEffector(appId, entityId, "unbind", "0", ImmutableMap.<String, Object>of(
+                 "app_guid", serviceInstanceBinding.getAppGuid()
+         ));
+         LOG.info("Calling unbind effector: {}", ServiceUtil.getFutureValueLoggingError(effector));
+         LOG.info("Deleting service binding: [BindingId={}]", bindingId);
+         bindingRepository.delete(bindingId);
+      }
+   }
 
 }
